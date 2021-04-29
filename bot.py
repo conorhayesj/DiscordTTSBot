ce22dc36060709ff7cd74ea50af924fc9931aabd
conorhayesj <conorhayesj@gmail.com>
2021-04-29 11:01:52 +0100

    adding comments

--- bot.py
+++ bot.py
@@ -22,6 +22,7 @@ async def on_ready():
 async def on_ready():
     print(f'{bot.user.name} has connected!')
 
+# ^tts will join the voice channel and use tts to say any of the arguments that followed the command. 
 @bot.command(name='tts')
 async def speech(ctx, *, ttstext):
     try:
@@ -36,18 +37,23 @@ async def speech(ctx, *, ttstext):
         vc = ctx.voice_client
 
     print(f'Name: {ctx.author.name} - Nick: {ctx.author.nick}')
+    # If the bot finds the author of the command is "Taz" or "Nan"
+    # it will instead play a different recording.
     if ctx.author.nick == 'Taz' or ctx.author.name == 'Nan':
         vc.play(discord.FFmpegPCMAudio(source='./other_sounds/taz.mp3'))
     else:
+        #Generate an mp3 using gTTS and store it.
         mytts = gTTS(text=ttstext, lang='en', slow=False)
         mytts.save('tts.mp3')
         vc.play(discord.FFmpegPCMAudio(source='./tts.mp3'))
     while vc.is_playing():
         await asyncio.sleep(1)
     await vc.disconnect()
+    #If the mp3 file exists, delete it to save space.
     if os.path.isfile('./tts.mp3'):
         os.remove('./tts.mp3')
 
+# ^stop to stop the bot and disconnect from Voice.
 @bot.command(name='stop')
 async def leave(ctx):
     try:
